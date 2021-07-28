74775f04c60932c4921c9730d5b8248107ba8f8c
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-07-28 09:12:10 +0300

    Add OutputSection markup and styles

--- src/components/OutputSection.js
+++ src/components/OutputSection.js
@@ -1,7 +1,29 @@
 import "./OutputSection.css";
 
-function OutputSection() {
-  return <div className="output-section">OutputSection</div>;
+function OutputSection({ handleReset }) {
+  return (
+    <div className="output-section">
+      <div className="output-flex">
+        <div>
+          <div className="output-title">Tip Amount</div>
+          <div className="output-subtitle">/ person</div>
+        </div>
+        <div className="output-value">$4.27</div>
+      </div>
+
+      <div className="output-flex">
+        <div>
+          <div className="output-title">Total</div>
+          <div className="output-subtitle">/ person</div>
+        </div>
+        <div className="output-value">$32.79</div>
+      </div>
+
+      <button className="reset-button" onclick={handleReset}>
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default OutputSection;
