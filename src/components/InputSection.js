ce70120a3ec4dfac95a27892d1509afae69bb07b
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-07-28 09:54:12 +0300

    Add InputSection markup and styles

--- src/components/InputSection.js
+++ src/components/InputSection.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
 import "./InputSection.css";
 
 function InputSection() {
-  return <div className="input-section">InputSection</div>;
+  const [billInput, setBillInput] = useState("");
+  const [numPeopleInput, setNumPeopleInput] = useState("");
+
+  const handleBillInputChange = (event) => {
+    // TODO: Error handling
+    setBillInput(event.target.value);
+  };
+
+  const handleNumPeopleInputChange = (event) => {
+    // TODO: Error handling
+    setNumPeopleInput(event.target.value);
+  };
+
+  return (
+    <div className="input-section">
+      <label className="input-label" htmlFor="billInput">
+        Bill
+      </label>
+      <br />
+      <input
+        name="billInput"
+        className="text-input bill-amount-text-input"
+        value={billInput}
+        onChange={handleBillInputChange}
+      />
+      <div>insert percentage options buttons here</div>
+      <div>insert custom percentage button here</div>
+      <label className="input-label" htmlFor="numPeopleInput">
+        Number of People
+      </label>
+      <br />
+      <input
+        className="text-input num-people-text-input"
+        name="numPeopleInput"
+        value={numPeopleInput}
+        onChange={handleNumPeopleInputChange}
+      />
+    </div>
+  );
 }
 
 export default InputSection;
