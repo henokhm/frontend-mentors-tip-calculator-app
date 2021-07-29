fcbec57b0040a0e380880b4e716b3222d2e2ec83
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-07-29 08:37:15 +0300

    Add logic to enter custom percentage

--- src/components/CustomPercentageInput.js
+++ src/components/CustomPercentageInput.js
@@ -1,12 +1,33 @@
 import "./CustomPercentageInput.css";
 
-function CustomPercentageInput() {
-  return (
-    <div>
-      <button className="custom-button">Custom</button>
-      <input className="custom-percentage-input" />
-    </div>
+function CustomPercentageInput({
+  userWillEnterCustomPercentage,
+  setUserWillEnterCustomPercentage,
+  setSelectedPercentage,
+}) {
+  const display = userWillEnterCustomPercentage ? (
+    <input
+      className="custom-percentage-input"
+      autoFocus
+      onBlur={(event) => {
+        const { value } = event.target;
+        if (value === "") {
+          setUserWillEnterCustomPercentage(false);
+        } else {
+          setSelectedPercentage(value);
+        }
+      }}
+    />
+  ) : (
+    <button
+      className="custom-button"
+      onClick={() => setUserWillEnterCustomPercentage(true)}
+    >
+      Custom
+    </button>
   );
+
+  return display;
 }
 
 export default CustomPercentageInput;
--- src/components/PercentageOptions.js
+++ src/components/PercentageOptions.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 import "./PercentageOptions.css";
 import SelectableButton from "./SelectableButton";
 import CustomPercentageInput from "./CustomPercentageInput";
 
 function PercentageOptions({ selectedPercentage, setSelectedPercentage }) {
+  const [userWillEnterCustomPercentage, setUserWillEnterCustomPercentage] =
+    useState(false);
+
   const percentageOptions = [5, 10, 15, 25, 50];
 
   return (
@@ -14,12 +19,16 @@ function PercentageOptions({ selectedPercentage, setSelectedPercentage }) {
           isSelected={selectedPercentage === value}
           handleSelected={(value) => {
             if (selectedPercentage !== value) {
-              setSelectedPercentage((prev) => value);
+              setSelectedPercentage(value);
             }
           }}
         />
       ))}
-      <CustomPercentageInput />
+      <CustomPercentageInput
+        userWillEnterCustomPercentage={userWillEnterCustomPercentage}
+        setUserWillEnterCustomPercentage={setUserWillEnterCustomPercentage}
+        setSelectedPercentage={setSelectedPercentage}
+      />
     </div>
   );
 }
